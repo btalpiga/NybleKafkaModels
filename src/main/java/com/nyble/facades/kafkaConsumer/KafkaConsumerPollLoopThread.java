8a4e2efe447226c33bb525a9e8efe62077216b8c
Bogdan <bogdan.talpiga@3nyble.com>
2020-11-28 18:09:51 +0200

    log error in kafka loop thread

--- src/main/java/com/nyble/facades/kafkaConsumer/KafkaConsumerPollLoopThread.java
+++ src/main/java/com/nyble/facades/kafkaConsumer/KafkaConsumerPollLoopThread.java
@@ -26,60 +26,64 @@ public class KafkaConsumerPollLoopThread<K,V> extends Thread{
 
     public void run(){
         ConsumerRecords<K, V> records;
-        while((records = k.poll(timeout)) != null){
-            if(processingType == KafkaConsumerFacade.PROCESSING_TYPE_SINGLE){
-                Map<TopicPartition, List<ConsumerRecord<K,V>>> processed = new HashMap<>();
-                for(ConsumerRecord<K, V> record: records){
-                    logger.debug("Start record processing");
+        try{
+            while((records = k.poll(timeout)) != null){
+                if(processingType == KafkaConsumerFacade.PROCESSING_TYPE_SINGLE){
+                    Map<TopicPartition, List<ConsumerRecord<K,V>>> processed = new HashMap<>();
+                    for(ConsumerRecord<K, V> record: records){
+                        logger.debug("Start record processing");
+                        try{
+                            if(!processor.process(record)){
+                                throw new Exception("code=kcplt_1");
+                            }
+                            processed.compute(new TopicPartition(record.topic(), record.partition()), (key, val)->{
+                                if(val == null){
+                                    val = new ArrayList<>();
+                                }
+                                val.add(record);
+                                return val;
+                            });
+                        }catch(Exception e){
+                            commitOffsets(k, new ConsumerRecords<>(processed));
+                            if(Objects.equals(e.getMessage(), "code=kcplt_1")){
+                                throw new RuntimeException(String
+                                        .format("Record %s at topic %s, partition %d, offset %d processing returned false",
+                                                record.value().toString(), record.topic(), record.partition(), record.offset()));
+                            }else{
+                                throw new RuntimeException(String
+                                        .format("Could not process record %s at topic %s, partition %d, offset %d",
+                                                record.value().toString(), record.topic(), record.partition(), record.offset()),
+                                        e);
+                            }
+                        }
+                    }
+                }else if(processingType == KafkaConsumerFacade.PROCESSING_TYPE_BATCH){
                     try{
-                        if(!processor.process(record)){
+                        if(!processor.processBatch(records)){
                             throw new Exception("code=kcplt_1");
                         }
-                        processed.compute(new TopicPartition(record.topic(), record.partition()), (key, val)->{
-                            if(val == null){
-                                val = new ArrayList<>();
-                            }
-                            val.add(record);
-                            return val;
-                        });
                     }catch(Exception e){
-                        commitOffsets(k, new ConsumerRecords<>(processed));
                         if(Objects.equals(e.getMessage(), "code=kcplt_1")){
-                            throw new RuntimeException(String
-                                    .format("Record %s at topic %s, partition %d, offset %d processing returned false",
-                                            record.value().toString(), record.topic(), record.partition(), record.offset()));
+                            throw new RuntimeException("Batch record processing failed");
                         }else{
-                            throw new RuntimeException(String
-                                    .format("Could not process record %s at topic %s, partition %d, offset %d",
-                                            record.value().toString(), record.topic(), record.partition(), record.offset()),
+                            throw new RuntimeException("Batch record processing failed",
                                     e);
                         }
                     }
                 }
-            }else if(processingType == KafkaConsumerFacade.PROCESSING_TYPE_BATCH){
-                try{
-                    if(!processor.processBatch(records)){
-                        throw new Exception("code=kcplt_1");
-                    }
-                }catch(Exception e){
-                    if(Objects.equals(e.getMessage(), "code=kcplt_1")){
-                        throw new RuntimeException(String
-                                .format("Batch record processing failed"));
-                    }else{
-                        throw new RuntimeException(String
-                                .format("Batch record processing failed"),
-                                e);
-                    }
-                }
-            }
 
-            try{
-                k.commitSync();
-            }catch(CommitFailedException e){
-                logger.error(e.getMessage(), e);
+                try{
+                    k.commitSync();
+                }catch(CommitFailedException e){
+                    logger.error(e.getMessage(), e);
+                }
+                logger.debug("End record processing");
             }
-            logger.debug("End record processing");
+        }catch(Exception e){
+            logger.error(e.getMessage(),e);
+            logger.error("Thread {} is stopping...", this.getName());
         }
+
     }
 
     private void commitOffsets(KafkaConsumer<K, V> consumer,ConsumerRecords<K, V> records){
